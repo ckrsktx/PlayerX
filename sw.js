e2a30b6ec91cca3585f845e5969aa5d7b73fa56b
ckrsktx <marciorocha.s@protonmail.com>
2025-09-26 09:12:58 -0300

    Update and rename 1990s.json to sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'retro-v1';
+const RAIZ = '/';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      RAIZ,
+      RAIZ + 'index.html',
+      RAIZ + 'app.css',
+      RAIZ + 'app.js',
+      RAIZ + 'manifest.json'
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(caches.match(RAIZ + 'index.html'));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
